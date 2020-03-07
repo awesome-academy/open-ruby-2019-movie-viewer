41f971b375c872c740fa0aeb2d265a88a4916db1
AnBinh <thanhbinhbk888@gmail.com>
2020-03-07 22:03:40 +0700

    Manage User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :ratings, dependent: :destroy
 
+  enum role: {user: 0, admin: 1}
+
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable
 end
--- db/migrate/20200305083509_add_type_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddTypeToUsers < ActiveRecord::Migration[6.0]
-  def up
-    add_column :users, :type, :string
-  end
-
-  def down
-    remove_column :users, :role
-  end
-end
