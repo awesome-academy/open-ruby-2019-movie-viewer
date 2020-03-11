ae513d1683a2ff1923f32851ba35997f68865053
phamhuuluan <luanphamm15@gmail.com>
2020-03-11 13:23:18 -0400

    Update destroy film

--- app/controllers/admins/films_controller.rb
+++ app/controllers/admins/films_controller.rb
@@ -1,10 +1,13 @@
-class Admins::FilmsController < ApplicationController
+class Admins::FilmsController < Admins::BaseController
+  before_action :load_film, only: %i(edit update destroy)
+
   def index
     @films = Film.create_at_desc
   end
 
   def new
     @film = Film.new
+    @film_profile = @film.build_film_profile
   end
 
   def create
@@ -19,9 +22,44 @@ class Admins::FilmsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @film.update update_film_params
+      flash[:success] = t ".success"
+      redirect_to admins_films_path
+    else
+      flash[:danger] = t ".danger"
+      render :edit
+    end
+  end
+
+  def destroy
+    @film.destroy
+
+    if @film.destroyed?
+      flash[:success] = t ".success_destroy_film"
+    else
+      flash[:danger] = t ".danger_destroy_film"
+    end
+    redirect_to request.referrer
+  end
+
   private
 
   def films_params
     params.require(:film).permit Film::FILM_PARAMS
+  end
+
+  def update_film_params
+    params.require(:film).permit Film::FILM_PARAMS
+  end
+
+  def load_film
+    @film = Film.find_by id: params[:id]
+
+    return if @film
+    flash[:danger] = t ".not_found_film" 
+    redirect_to root_path
   end
 end
