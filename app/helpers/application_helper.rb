7aea4c3d31f54d07b32d66d5e0eaa2c8e1031468
AnBinh <thanhbinhbk888@gmail.com>
2020-03-23 06:42:26 +0700

    comment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def list_film category_id
     @films = Film.where(category_id: category_id).limit Settings.limmit_films_home
   end
+
+  def list_comment film_id
+    @comments = Comment.list_comment(film_id).order_comment
+  end
 end
--- app/models/film.rb
+++ app/models/film.rb
@@ -5,6 +5,7 @@ class Film < ApplicationRecord
   belongs_to :category
   has_one :film_profile, dependent: :destroy
   has_many :comments
+  has_many :film_comments, class_name: Comment.name, foreign_key: :film_id
   has_many :ratings
   has_many :actor_films
   has_one_attached :movie
